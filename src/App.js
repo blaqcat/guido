4e2e88d4eec2cbbc4d1ca78c217f5f9766d52fc7
blaqcat <ngakana@protonmail.com>
2021-09-26 13:50:23 +0200

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import SearchBar from "./Components/SearchBar";
+import { Amplify } from "aws-amplify";
 import BookData from "./Data.json";
+import  { getAyoba }  from './microapp';
+import config from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import guidoImage from "./guido.jpg";
+
+Amplify.configure(config)
 
 function App() {
+
+  const [phone, setPhone] = useState('');
+  const sayHello = () => {
+
+   
+    let ayoba = getAyoba() ; 
+    setPhone(ayoba.getMsisdn());
+
+  } 
+
+
+
   return (
-    <div className="App">
+    <div
+     className="App"
+     style={{
+       backgroundImage: 'url('+guidoImage+')',
+     }}
+     >
       <SearchBar placeholder="Search for Opportunity..." data={BookData} />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
